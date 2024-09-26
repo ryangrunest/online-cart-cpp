8ce945a65a787375c35c37c512b7d7a1b1afb833
Ryan Grunest <ryangrunest@geeko>
2024-09-26 17:52:48 +0200

    create class declaration

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -4,6 +4,17 @@
 #include <string>
 using namespace std;
 
-/* Type your code here */
+class ItemToPurchase {
+  public:
+    ItemToPurchase(string itemName = "none", int itmePrice = 0, int itemQuantity = 0);
+    void SetName();
+    void GetName();
+    void SetQuantity();
+    void GetQuantity();
+  private:
+    string itemName;
+    int itemPrice;
+    int itemQuantity;
+};
 
 #endif
